b1ac1c95d076a93150ba508027f3f0d88064d653
ShubhanakarCoder <107795000+ShubhanakarCoder@users.noreply.github.com>
2023-03-13 19:17:48 +0530

    book-list application

---
+++ src/App.js
@@ -0,0 +1,34 @@
+// import logo from './logo.svg';
+// import './App.css';
+// import Navbar from './Components/Navbar';
+
+// function App() {
+//   return (
+//     <Navbar/>
+//   );
+// }
+
+// export default App;
+
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Components/Navbar';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SearchResult from './Components/SearchResult';
+
+function App() {
+  return (
+    <>
+      {/* <Navbar/> */}
+     
+                <Routes>
+                    <Route path="/" exact element={<Navbar />} />
+                    <Route path="/:query/:startIndex" exact element={<SearchResult  />} />
+                </Routes>
+        
+    </>
+    
+  );
+}
+
+export default App;
